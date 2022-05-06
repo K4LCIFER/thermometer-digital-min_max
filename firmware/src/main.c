078ea52a5090e1dfcba0f9496022bf6c3481fe17
Kalcifer <7829231+K4LCIFER@users.noreply.github.com>
2022-05-05 23:50:34 -0700

    Got the data communication working. Issues with interrupt flags, compiler optimizations not being enabled, and many other minor bugs.

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -4,115 +4,61 @@
 /* System Defines */
 #define __AVR_ATmega328P__
 #define F_CPU 8000000UL
-#define INND_TS56
 /* AVR Includes */
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-/* GCC Includes */
-#include <stdbool.h>
-/* Custom Includes */
-#include "seven_segment.h"
-/* Program Defines */
-// TODO: Need to find a way to clean up the pins and ports with something like
-// this.
-
-#define SS_SEGMENTS_DIRECTION_PORT DDRD
-#define SS_SEGMENTS_OUTPUT_PORT PORTD
-
-#define SS_DISPLAY_ENABLE_DIRECTION_PORT DDRC
-#define SS_DISPLAY_ENABLE_OUTPUT_PORT PORTC
-#define SS_DISPLAY_0_ENABLE_PIN 0
-#define SS_DISPLAY_1_ENABLE_PIN 1
-#define SS_DISPLAY_2_ENABLE_PIN 2
-#define SS_DISPLAY_3_ENABLE_PIN 3
 
 #define SENSOR_DATA_DIRECTION_PORT DDRB
 #define SENSOR_DATA_OUTPUT_PORT PORTB
 #define SENSOR_DATA_INPUT_PORT PINB
 #define SENSOR_DATA_PIN 0
 
-// uint16_t received_temperature
-// NOTE: volatile because they are being accessed and modified by interrupts.
+//base[0] = PIN, base[1] = DDR, base[2] = PORT, pinbm = pin bitmask
+typedef struct
+{
+    uint8_t* register;
+    uint8_t bitmask;
+} pin_t;
+
+//pins
+static const pin_t sensor_pin = { &PINB, 1 << 0 }; //PB1
+
 volatile uint8_t number_of_received_bits = 0;
 volatile uint64_t raw_sensor_data = 0;
-volatile bool transmission_complete = true;
-// uint16_t temperature;
-uint8_t parsed_temperature[4];
 
-uint8_t rising_edge_counter = 0;
-uint16_t timestamps[40];
-uint8_t falling_edge_counter = 0;
-
-struct SensorData
+union SensorData
 {
-    uint16_t humidity;
-    uint16_t temperature;
-    uint8_t checksum_data;
+    uint64_t raw_sensor_data;
+    struct
+    {
+        uint16_t humidity_data;
+        uint16_t temperature_data;
+        uint16_t checksum;
+    }
 };
 
 
 void init(void);
 uint64_t sensor_rx(void);
-void parse_sensor_data(uint64_t data);
-// void _display_digit(uint8_t digit);
-void display_temperature(uint8_t temperature[]);
-
-
-// struct ReceivedData;
 
 void main(void)
 {
     init();
 
-    // The startup time of the sensor appears to be around 750us, but the
-    // datasheet also says that it requires 2s between transmissions. The
-    // following just prevents any data from being pulled from the sensor right
-    // away after startup.
-    // SS_SEGMENTS_OUTPUT_PORT = 0xFF;
-    SS_DISPLAY_ENABLE_OUTPUT_PORT |= (1 << SS_DISPLAY_0_ENABLE_PIN);
-    _delay_ms(2000);
-    uint8_t d = sensor_rx();
-    if (d == 0)
-    {
-        PORTB |= (1 << DDB7);
-    }
-    parse_sensor_data(raw_sensor_data);
-    display_temperature(parsed_temperature);
-    while(1){}
+    _delay_ms(2000);    // Sensor bootup delay.
+
+    sensor_rx();
+
+    while(1){};
 }
 
 
 void init(void)
 {
-    // Set all of the segment pins as outputs (they're all on port d)
-    // SS_SEGMENTS_DIRECTION_PORT = 0b11111111;
-    DDRD &= ~(1 << DDD5);    // external timer
-
-    // Set pins controlling the transistor multiplexers as outputs.
-    // <display-2><display-1><display-0>
-    // Enable Display 0
-    SS_DISPLAY_ENABLE_DIRECTION_PORT |= (1 << SS_DISPLAY_0_ENABLE_PIN);
-    // Enable Display 1
-    SS_DISPLAY_ENABLE_DIRECTION_PORT |= (1 << SS_DISPLAY_1_ENABLE_PIN);
-    // Enable Display 2
-    SS_DISPLAY_ENABLE_DIRECTION_PORT |= (1 << SS_DISPLAY_2_ENABLE_PIN);
-    // (Need a 4th display.)
-
     // Set up the interrupts:
     sei();  // Enable global interrupts.
-    // PCICR |= (1 << PCIE0);  // Enable the interrupts for PCINT[7:0].
-
-    // NOTE: Perhaps only enable the below interrupt, the moment that you are
-    // going to use it to prevent unwanted calls.
     PCMSK0 |= (1 << PCINT0);    // Enable the interrupt for pin B1.
-
-
-    // Set up the timer for use with the receive function:
-    // TCCR1B |= (1 << CS11);  // CLK/8 prescaler
-    TCCR1B |= (1 << ICES1); // Set ICP1 to capture a falling edge.
-    TCCR1B |= (1 << ICNC1); // Enable the Input capture noise canceller.
-
 
     DDRB |= (1 << DDB7); // Test led
 }
@@ -122,7 +68,6 @@ uint64_t sensor_rx(void)
 {
     raw_sensor_data = 0;
     number_of_received_bits = 0;
-    transmission_complete = false;
 
     // struct SensorData received_data;
     // Pull low 1ms minimum. This is done first so that the line isn't loaded
@@ -138,73 +83,14 @@ uint64_t sensor_rx(void)
         return 0; // Exit out of the function.
     }
     _delay_us(135); // Wait 135us to go into the midle of the start data transmission bit.
+    // Clear the interrupt flags:
+    TIFR1 |= (1 << ICF1);
+    PCIFR |= (1 << PCIF0);
     PCICR |= (1 << PCIE0);  // Enable the rising edge interrupt.
+    TIMSK1 |= (1 << ICIE1); // Enable the timer capture interrupt.
 
-    while (!transmission_complete); // Wait here for the interrupt service routine to acquire data.
-
+    while (number_of_received_bits < 40); // Wait here for the interrupt service routine to acquire data.
     return raw_sensor_data;
-}
-
-void parse_sensor_data(uint64_t data)
-{
-    bool is_negative = false;
-    // bit shift to the right 8 to remove the checksum, and AND the first 16 bits
-    // to remove the humiditiy data leaving the middle 16 bits representing the 
-    // temperature.
-    uint16_t temperature_data = (data >> 8) & 0x0000FFFF;   // Isolate the temperature bits
-
-    // The first bit of the temperature data represents a negative temperature.
-    if (temperature_data & (1 << 15))
-    {
-        parsed_temperature[3] = 1;
-    }
-    // Separate the returned temperature into an array of its digits so that it
-    // is easier to display them on a seven segment.
-    uint8_t temperature_digit_position = 0;
-    while (temperature_digit_position <= 2)
-    {
-        parsed_temperature[temperature_digit_position] = temperature_data % 10;
-        temperature_data = temperature_data / 10;
-        temperature_digit_position++;
-    }
-}
-
-
-void display_temperature(uint8_t* temperature)
-{
-    // switch (temperature[3])
-    // {
-        // case 0:
-            // SS_SEGMENTS_OUTPUT_PORT = SS_0;
-            // break;
-        // case 1:
-            // SS_SEGMENTS_OUTPUT_PORT = SS_1;
-            // break;
-        // case 2:
-            // SS_SEGMENTS_OUTPUT_PORT = SS_2;
-            // break;
-        // case 3:
-            // SS_SEGMENTS_OUTPUT_PORT = SS_3;
-            // break;
-        // case 4:
-            // SS_SEGMENTS_OUTPUT_PORT = SS_4;
-            // break;
-        // case 5:
-            // SS_SEGMENTS_OUTPUT_PORT = SS_5;
-            // break;
-        // case 6:
-            // SS_SEGMENTS_OUTPUT_PORT = SS_6;
-            // break;
-        // case 7:
-            // SS_SEGMENTS_OUTPUT_PORT = SS_7;
-            // break;
-        // case 8:
-            // SS_SEGMENTS_OUTPUT_PORT = SS_8;
-            // break;
-        // case 9:
-            // SS_SEGMENTS_OUTPUT_PORT = SS_9;
-            // break;
-    // }
 }
 
 // Interrupt Service routine for the start of a data bit
@@ -214,9 +100,9 @@ ISR (PCINT0_vect)
     // Interrupt flag is automatically cleared when the ISR is called.
     // Start the timer (starts at beginning of the bit)
     TCNT1 = 0;  // Clear the timer.
-    // TCCR1B |= (1 << CS11);  // Start the timer using CLK/8
-    TCCR1B |= ((1 << CS12) | (1 << CS11) | (1 << CS10));  // External clock
+    TCCR1B |= (1 << CS11);  // Start the timer using CLK/8
 
+    PCIFR |= (1 << PCIF0);
     TIMSK1 |= (1 << ICIE1); // Enable the timer capture interrupt.
     PCICR &= ~(1 << PCIE0); // Disable the PCINT0 interrupt.
 }
@@ -226,14 +112,9 @@ ISR (TIMER1_CAPT_vect)
 {
     uint8_t captured_timer_value = ICR1;
     falling_edge_counter++;
-    // float bit_length = ICR1 * TIMER_COUNT_PERIOD;
     timestamps[number_of_received_bits] = captured_timer_value;
     raw_sensor_data <<= 1;
-    if (captured_timer_value < 45)    // Is a 0
-    {
-        // Do nothing. Doing nothing is the same as writing a zero.
-    }
-    else if (captured_timer_value > 45)   // Is a 1
+    if (captured_timer_value > 45)   // Is a 1
     {
         raw_sensor_data |= 1;
     }
@@ -243,6 +124,7 @@ ISR (TIMER1_CAPT_vect)
     // Continue the capturing if 40 bits have not been acquired yet:
     if (number_of_received_bits < 40)
     {
+        PCIFR |= (1 << PCIF0);
         TIMSK1 &= ~(1 << ICIE1);    // Disable the input capture interrupt.
         PCICR |= (1 << PCIE0); // Enable the PCINT0 interrupt on pin B0
     }
@@ -251,7 +133,6 @@ ISR (TIMER1_CAPT_vect)
         // Disable both interrupts:
         TIMSK1 &= ~(1 << ICIE1);
         PCICR &= ~(1 << PCIE0);
-        transmission_complete = true;
     }
 }
 
